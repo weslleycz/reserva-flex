733de82f78d02cdbc1cf14b819c3813209faf3a3
weslley <weslleysouza22@gmail.com>
2023-05-24 19:51:02 -0300

    🔧 Configurando o swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Exemplo')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Retorna Hello World!' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Exemplo de API')
+    .setDescription('Descrição da API')
+    .setVersion('1.0')
+    .addTag('Exemplo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
